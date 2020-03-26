214d5d6cc2a99713e6fb36af7a72acab07e7d660
sbouaziz-lvmh <soukaina.bouaziz@gmail.com>
2020-03-26 13:08:22 +0000

    Updates

---
+++ precache-manifest.a6258381be23e2a4db37e1f4d4ac8f93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10af983b8411a9132fc8aad85a0b3272",
+    "url": "/store-crowd-covid/index.html"
+  },
+  {
+    "revision": "eca9bfeaffb6c952c9b5",
+    "url": "/store-crowd-covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3fa03ae40360340106e2",
+    "url": "/store-crowd-covid/static/js/2.4636d54b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-crowd-covid/static/js/2.4636d54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca9bfeaffb6c952c9b5",
+    "url": "/store-crowd-covid/static/js/main.170deea6.chunk.js"
+  },
+  {
+    "revision": "333cda4dfb23f6e4408b",
+    "url": "/store-crowd-covid/static/js/runtime-main.1765905d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/store-crowd-covid/static/media/logo.5d5d9eef.svg"
+  }
+]);
